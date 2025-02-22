2d6e78b2b061aa384af504932a11fa3fde38896d
0xidennis <156088691+0xidennis@users.noreply.github.com>
2025-02-22 12:56:26 -0800

    first commit

--- src/Static/Footer.jsx
+++ src/Static/Footer.jsx
@@ -6,34 +6,34 @@ const Footer = () => {
       <nav className="container mx-auto px-4">
         <ul className="flex flex-wrap items-center justify-center gap-2 text-sm">
           <li>
-            <Link to="/" className="hover:text-amber-300 text-red-600">Home</Link>
+            <Link to="/" className="hover:text-amber-300 text-red-600 text-[0.9rem] font-semibold">Home</Link>
           </li>
           
           <li className=" md:block text-black">|</li>
           <li>
-            <Link to="https://www.escortsaffair.com/centre/terms/contact" className="hover:text-amber-300 text-red-600">Contact</Link>
+            <Link to="https://www.escortsaffair.com/centre/terms/contact" className="hover:text-amber-300 text-red-600 text-[0.9rem] font-semibold">Contact</Link>
           </li>
 
           <li className=" md:block text-black">|</li>
           <li>
-            <Link to="https://www.escortsaffair.com/centre/terms/privacy" className="hover:text-amber-300 text-red-600">Privacy</Link>
+            <Link to="https://www.escortsaffair.com/centre/terms/privacy" className="hover:text-amber-300 text-red-600 text-[0.9rem] font-semibold">Privacy</Link>
           </li>
 
           <li className=" md:block text-black">|</li>
           <li>
-            <Link to="https://www.escortsaffair.com/centre/terms/terms" className="hover:text-amber-300 text-red-600">Terms</Link>
+            <Link to="https://www.escortsaffair.com/centre/terms/terms" className="hover:text-amber-300 text-red-600 text-[0.9rem] font-semibold">Terms</Link>
           </li>
 
           <li className=" md:block text-black">|</li>
           <li>
-            <Link to="https://www.escortsaffair.com/centre/terms/report" className="hover:text-amber-300 text-red-600">Report Traffic</Link>
+            <Link to="https://www.escortsaffair.com/centre/terms/report" className="hover:text-amber-300 text-red-600 text-[0.9rem] font-semibold">Report Traffic</Link>
           </li>
 
           <li className=" md:block text-black">|</li>
           <li>
             <span className="text-gray-600">
               © 2022{" "}
-              <a href="/" className="text-red-600 hover:text-amber-300">
+              <a href="/" className="text-red-600 hover:text-amber-300 text-[0.9rem] font-semibold">
                 EscortsAffairManagement.com
               </a>
             </span>
